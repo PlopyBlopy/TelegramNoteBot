c054b0d6ba76a480de011b6957b0adf71cdf95e2
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-11-06 19:26:54 +0300

    feat(logger): настройки для логирование с пакетом zerolog

---
+++ pkg/logger/logger.go
@@ -0,0 +1,13 @@
+package logger
+
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func NewLogger() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
+}
