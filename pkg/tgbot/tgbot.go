e9010a48fab1c94e3a08f0d73c8bd189ab9d7828
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-11-06 14:53:08 +0300

    feat(tgbot): определены базовые функции для прослушивания сообщений через polly - Config - настройки env - Bot - необходимые зависимости для tg api и маршрутизатора сообщений - NewBot - создание бота - Run - запуск и прослушивание сообщений

---
+++ pkg/tgbot/tgbot.go
@@ -0,0 +1,54 @@
+package tgbot
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/PlopyBlopy/notebot/internal/router"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/rs/zerolog/log"
+)
+
+type Config struct {
+	Token   string `env:"BOT_TOKEN"`
+	Timeout string `env:"BOT_TIMEOUT"`
+}
+
+type Bot struct {
+	api    *tgbotapi.BotAPI
+	router *router.Router
+}
+
+func NewBot(router *router.Router, c Config) (*Bot, error) {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		return nil, fmt.Errorf("couldn't create a new BotAPI. %s", err)
+	}
+
+	log.Info().Msgf("Authorized on account %s", bot.Self.UserName)
+
+	return &Bot{
+		api:    bot,
+		router: router,
+	}, nil
+}
+
+func (b *Bot) Run() error {
+	u := tgbotapi.NewUpdate(0)
+
+	timeout, err := strconv.Atoi(os.Getenv("BOT_TIMEOUT"))
+	if err != nil {
+		return fmt.Errorf("timeout value invalid. %w", err)
+	}
+
+	u.Timeout = timeout
+
+	updates := b.api.GetUpdatesChan(u)
+
+	for update := range updates {
+		b.router.HandleUpdate(b.api, update)
+	}
+
+	return nil
+}
