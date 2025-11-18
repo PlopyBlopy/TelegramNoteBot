80463ac9c56f9fa97c4389c527a67087115bed45
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-11-18 07:55:08 +0300

    feat(modal): компонент для отображения модального окна, с базовым css

---
+++ Frontend/src/shared/components/modal/modal.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react";
+import styles from "./modal.module.css";
+import { createPortal } from "react-dom";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  if (!isOpen) return null;
+
+  return createPortal(
+    <div className={styles.modalOverlay} onClick={onClose}>
+      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>,
+    document.body,
+  );
+};
