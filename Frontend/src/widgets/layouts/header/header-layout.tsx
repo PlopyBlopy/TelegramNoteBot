84912f3dd264541e92d8267c7e1262f7ceeee521
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-11-18 07:23:36 +0300

    feat(header-layout): стилизация header, добавлена иконка, смена темы

--- Frontend/src/widgets/layouts/header/header-layout.tsx
+++ Frontend/src/widgets/layouts/header/header-layout.tsx
@@ -1,9 +1,15 @@
+import { ThemeToggle } from "@/widgets/theme-toggle";
 import styles from "./header-layout.module.css";
+import { Icon } from "@/shared/components/icon";
+import { Icons } from "@/shared/assets/icons";
 
 export const HeaderLayout = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.items}></div>
+      <div className={styles.items}>
+        <Icon IconComponent={Icons.elements.note} />
+        <ThemeToggle />
+      </div>
     </div>
   );
 };
