3b10f07c64b15244d359fdeca1f3f5f4264064fa
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-11-18 22:52:29 +0300

    feat(note_service, B): сервис для CRUD с note использование в usecase

---
+++ Backend/internal/adapters/note/service/note_service.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"time"
+
+	"github.com/PlopyBlopy/notebot/internal/adapters/note"
+)
+
+type INoteManager interface {
+	AddNote(title, description string, themeId, noteColorId int, tagIds ...int) error
+}
+
+type CreateNote struct {
+	Title       string
+	Description string
+	ThemeId     int
+	TagIds      []int
+	NoteColorId int
+}
+
+type NoteCard struct {
+	Note        note.Note
+	Completed   bool
+	ThemeId     int
+	TagsId      []int
+	NoteColorId int
+	CreatedAt   time.Time
+}
+
+type NoteService struct {
+	noteManager INoteManager
+}
+
+func NewNoteService(nm INoteManager) (*NoteService, error) {
+	return &NoteService{noteManager: nm}, nil
+}
