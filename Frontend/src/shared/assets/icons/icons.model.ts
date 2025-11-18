4c3e03b16616ac998d4e6c28c6a0eac3a95dc020
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-11-18 07:17:47 +0300

    feat(assets): папка с иконками формата svg и файла для получения иконок

---
+++ Frontend/src/shared/assets/icons/icons.model.ts
@@ -0,0 +1,22 @@
+import DefaultIcon from "@/shared/assets/icons/default_icon.svg?react";
+import EditIcon from "@/shared/assets/icons/edit.svg?react";
+import DeleteIcon from "@/shared/assets/icons/delete.svg?react";
+import SunIcon from "@/shared/assets/icons/sun.svg?react";
+import MoonIcon from "@/shared/assets/icons/moon.svg?react";
+import NoteIcon from "@/shared/assets/icons/note.svg?react";
+import PlusIcon from "@/shared/assets/icons/plus.svg?react";
+import CloseIcon from "@/shared/assets/icons/close.svg?react";
+
+export const Icons = {
+  default: DefaultIcon,
+
+  elements: {
+    edit: EditIcon,
+    delete: DeleteIcon,
+    sun: SunIcon,
+    moon: MoonIcon,
+    note: NoteIcon,
+    plus: PlusIcon,
+    close: CloseIcon,
+  },
+};
